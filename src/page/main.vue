037af5fbd93140091a57b523bfbb17e04b555ac4
PPAmaranth <523002128@qq.com>
2019-05-10 17:33:46 +0800

    'rebuild'

--- src/page/main.vue
+++ src/page/main.vue
@@ -1,20 +1,55 @@
 <template>
   <div>
-      main
+      <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+        <el-aside width="200px">
+            <navpage></navpage>
+        </el-aside>
+        <el-main>Main</el-main>
+    </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import navpage from '@/page/nav'
+
 export default {
-  name: 'main',
+  name: 'mainpage',
   data () {
     return {
-      msg: '11'
+      
     }
+  },
+  components:{
+      navpage
   }
 }
 </script>
 
 <style scoped>
-
+.el-container{
+    height:100vh;
+}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
 </style>
