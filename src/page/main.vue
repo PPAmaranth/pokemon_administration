8470f564633589ffbb2fa23cf90af321092bf03c
PPAmaranth <523002128@qq.com>
2019-05-10 15:10:44 +0800

    init

---
+++ src/page/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      main
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return {
+      msg: '11'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
